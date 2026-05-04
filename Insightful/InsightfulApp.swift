54b87d5c350c9a1d4dd5a6ff4125a8ea67782c41
Andy <wongandy97@gmail.com>
2026-05-03 21:05:59 -0400

    start project

---
+++ Insightful/InsightfulApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsightfulApp.swift
+//  Insightful
+//
+//  Created by Andy Wong on 5/3/26.
+//
+
+import SwiftUI
+
+@main
+struct InsightfulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InsightfulTests/InsightfulTests.swift
@@ -0,0 +1,17 @@
+//
+//  InsightfulTests.swift
+//  InsightfulTests
+//
+//  Created by Andy Wong on 5/3/26.
+//
+
+import Testing
+@testable import Insightful
+
+struct InsightfulTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
